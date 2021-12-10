e78dd58aa950efe2db8e66472af498fd42ebc1c5
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-12-09 22:30:02 -0500

    2

---
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const CoinDetailedScreen = () => {
+  return (
+    <View>
+      <Text style={{ color: "white" }}>Ayooooo</Text>
+    </View>
+  );
+};
+
+export default CoinDetailedScreen;
