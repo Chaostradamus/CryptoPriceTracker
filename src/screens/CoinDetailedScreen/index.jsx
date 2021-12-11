818c58c1a69a90224e750959ef0171be42feeb4b
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-12-11 17:03:18 -0500

    prefolder structure

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import { Ionicons, EvilIcons } from "@expo/vector-icons";
+import Coin from "../../../assets/data/crypto.json";
+import styles from "./styles";
 
 const CoinDetailedScreen = () => {
+  const {
+    image: { small },
+    name,
+    symbol,
+    market_data: { market_cap_rank },
+  } = Coin;
+
   return (
     <View>
-      <Text style={{ color: "white" }}>Ayooooo</Text>
+      <View style={styles.headerContainer}>
+        <Ionicons name="chevron-back-sharp" size={24} color="white" />
+
+        <View style={styles.tickerContainer}>
+          <Image source={{ uri: small }} style={{ width: 26, height: 26 }} />
+          <Text style={styles.tickerTitle}>{symbol.toUpperCase()}</Text>
+
+          <View style={styles.rankContainer}>
+            <Text style={{ color: "white", fontWeight: "bold", fontSize: 15 }}>
+              #{market_cap_rank}
+            </Text>
+          </View>
+        </View>
+        <EvilIcons name="user" size={30} color="white" />
+      </View>
     </View>
   );
 };
