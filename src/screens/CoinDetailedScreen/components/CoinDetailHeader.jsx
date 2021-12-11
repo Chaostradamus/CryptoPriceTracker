facdd8131a82717c19049915f5c2847af404aa5b
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-12-11 17:22:26 -0500

    post folder structure

--- src/screens/CoinDetailedScreen/components/CoinDetailHeader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CoinDetailedHeader = () => {
-return (
-    
-)
-}
-
-export default CoinDetailedHeader
--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
-import { Ionicons, EvilIcons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
 import Coin from "../../../assets/data/crypto.json";
-import styles from "./styles";
+import CoinDetailedHeader from "./components/CoinDetailedHeader/index";
 
 const CoinDetailedScreen = () => {
   const {
@@ -14,21 +13,12 @@ const CoinDetailedScreen = () => {
 
   return (
     <View>
-      <View style={styles.headerContainer}>
-        <Ionicons name="chevron-back-sharp" size={24} color="white" />
-
-        <View style={styles.tickerContainer}>
-          <Image source={{ uri: small }} style={{ width: 26, height: 26 }} />
-          <Text style={styles.tickerTitle}>{symbol.toUpperCase()}</Text>
-
-          <View style={styles.rankContainer}>
-            <Text style={{ color: "white", fontWeight: "bold", fontSize: 15 }}>
-              #{market_cap_rank}
-            </Text>
-          </View>
-        </View>
-        <EvilIcons name="user" size={30} color="white" />
-      </View>
+      <CoinDetailedHeader
+        image={small}
+        
+        symbol={symbol}
+        marketCapRank={market_cap_rank}
+      />
     </View>
   );
 };
