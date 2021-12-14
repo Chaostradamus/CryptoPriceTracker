e9e248698463141ad87066c9687bd15254de54cf
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-12-13 23:52:45 -0500

    216

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -2,23 +2,27 @@ import React from "react";
 import { Text, View } from "react-native";
 import Coin from "../../../assets/data/crypto.json";
 import CoinDetailedHeader from "./components/CoinDetailedHeader/index";
+import styles from "./styles";
 
 const CoinDetailedScreen = () => {
   const {
     image: { small },
     name,
     symbol,
-    market_data: { market_cap_rank },
+    market_data: { market_cap_rank, current_price },
   } = Coin;
 
   return (
-    <View>
+    <View style={{ paddingHorizontal: 10 }}>
       <CoinDetailedHeader
         image={small}
-        
         symbol={symbol}
         marketCapRank={market_cap_rank}
       />
+      <View>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.currentPrice}>${current_price.usd}</Text>
+      </View>
     </View>
   );
 };
