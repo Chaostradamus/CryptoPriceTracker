1f8daf214ad6bb120c21908be8b5226cac4d9ac2
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-12-14 01:12:06 -0500

    charts

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,28 +1,78 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Dimensions } from "react-native";
 import Coin from "../../../assets/data/crypto.json";
 import CoinDetailedHeader from "./components/CoinDetailedHeader/index";
 import styles from "./styles";
+import { AntDesign } from "@expo/vector-icons";
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+} from "@rainbow-me/animated-charts";
 
 const CoinDetailedScreen = () => {
   const {
     image: { small },
     name,
     symbol,
-    market_data: { market_cap_rank, current_price },
+    prices,
+    market_data: {
+      market_cap_rank,
+      current_price,
+      price_change_percentage_24h,
+    },
   } = Coin;
+
+  const percentageColor =
+    price_change_percentage_24h < 0 ? "#ea3943" : "#16c784";
+
+  const screenWidth = Dimensions.get("window").width;
 
   return (
     <View style={{ paddingHorizontal: 10 }}>
-      <CoinDetailedHeader
-        image={small}
-        symbol={symbol}
-        marketCapRank={market_cap_rank}
-      />
-      <View>
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.currentPrice}>${current_price.usd}</Text>
-      </View>
+      <ChartPathProvider
+        data={{
+          points: prices.map((price) => ({ x: price[0], y: price[1] })),
+          smoothingStrategy: "bezier",
+        }}
+      >
+        <CoinDetailedHeader
+          image={small}
+          symbol={symbol}
+          marketCapRank={market_cap_rank}
+        />
+        <View style={styles.priceContainer}>
+          <View>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.currentPrice}>${current_price.usd}</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: percentageColor,
+              paddingHorizontal: 3,
+              paddingVertical: 8,
+              borderRadius: 5,
+              flexDirection: "row",
+            }}
+          >
+            <AntDesign
+              name={price_change_percentage_24h < 0 ? "caretdown" : "caretup"}
+              size={12}
+              color={"white"}
+              style={{ alignSelf: "center", marginRight: 5 }}
+            />
+            <Text style={styles.priceChange}>
+              {price_change_percentage_24h.toFixed(2)}%
+            </Text>
+          </View>
+        </View>
+        <ChartPath
+          height={screenWidth / 2}
+          stroke="yellow"
+          width={screenWidth}
+        />
+        <ChartDot style={{ backgroundColor: "blue" }} />
+      </ChartPathProvider>
     </View>
   );
 };
--- src/screens/CoinDetailedScreen/styles.js
+++ src/screens/CoinDetailedScreen/styles.js
@@ -11,6 +11,17 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 15,
   },
+  priceContainer: {
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  priceChange: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: "400",
+  },
 });
 
 export default styles;
